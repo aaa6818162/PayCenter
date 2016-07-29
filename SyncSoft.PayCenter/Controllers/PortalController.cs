12923154fd523d74e662fd778eb1c7d44a959d70
awpli <a6818162@163.com>
2016-07-29 16:20:06 +0800

    111

---
+++ SyncSoft.PayCenter/Controllers/PortalController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using SyncSoft.PayCenterSdk;
+using SyncSoft.PayCenterSdk.Model;
+using SyncSoft.PayCenterSdk.Request;
+using SyncSoft.PayCenterSdk.Response;
+
+namespace SyncSoft.PayCenter.Controllers
+{
+    public class PortalController : Controller
+    {
+
+        public ActionResult Pay()
+        {
+            return View();
+        }
+
+        public ActionResult ConfrimPay(PayEnum PayType)
+        {
+            //建立请求
+            PayCenterRequest request = TestDictionary.GetTestByPartnerId();
+            string requestFrom = new PayCenterClient(PayType).GetRequestHtml(request);
+            return Content(requestFrom);
+        }
+
+
+        public ActionResult Index()
+        {
+            var t = new PayCenterClient(PayEnum.PayCenter).GetRequestResult().PayCenterResponse;
+            if (string.IsNullOrEmpty(t.sign)
+                || string.IsNullOrEmpty(t.sign_type)
+                || string.IsNullOrEmpty(t.OrderNo)
+                || string.IsNullOrEmpty(t.Partner)
+                || string.IsNullOrEmpty(t.UserId)
+                || string.IsNullOrEmpty(t.UserName)
+                || string.IsNullOrEmpty(t.PayRemark)
+                )
+            {
+                ViewBag.Message = "当前交易请求缺少必要的参数";
+                return View();
+            }
+
+            //if ((DateTime.Now - submittime).TotalSeconds > 60)
+            //{
+            //    ViewBag.Message = "当前交易请求已超时";
+            //    return View();
+            //}
+
+            var config = TestDictionary.GetTestByPartnerId();
+            PayCenterRequest request = t;
+            t.PartnerConfig = TestDictionary.GetTestByPartnerId().PartnerConfig;
+
+            //验证数据是否已被篡改
+            if (!new PayCenterClient(t.PayType).SignVerify(request, t.sign))
+            {
+                ViewBag.Message = "数据已被篡改";
+                return View();
+            }
+            return View("Pay");
+        }
+
+
+        //public ActionResult Index(string request )
+        //{
+        //    var reuestEntity = JsonConvert.DeserializeObject<PayCenterRequest>(request);
+
+        //    if ((DateTime.Now - reuestEntity.SubmitTime).TotalSeconds > 60)
+        //    {
+        //        ViewBag.Message = "当前交易请求已超时";
+        //        return View();
+        //    }
+
+        //    //验证数据是否已被篡改
+        //    if (!new PayCenterClient(reuestEntity.PayType).SignVerify(reuestEntity, reuestEntity.Sign))
+        //    {
+        //        ViewBag.Message = "数据已被篡改";
+        //        return View();
+        //    }
+
+
+        //    return View("Pay");
+        //}
+
+
+
+
+
+        #region 回调页面
+
+        /// <summary>
+        /// 支付宝同步处理
+        /// </summary>
+        /// <returns></returns>
+
+        public ActionResult AlipayReturn()
+        {
+            var entity = new PayCenterClient(PayEnum.Alipay).GetRequestResult("支付宝同步");
+            //获取配置信息
+            PayCenterRequest request = TestDictionary.GetTestByPartnerId(entity.AlipayResponse.Partner);
+            request.ThirdPayResponse = entity;
+
+            if (!new PayCenterClient(PayEnum.Alipay).SignVerify(request, entity.AlipayResponse.Sign))
+            {
+                ViewBag.Message = "数据已被篡改";
+                return View();
+            }
+
+            if (!entity.AlipayResponse.IsSuccess)
+            {
+                ViewBag.Message = "交易失败";
+                return View();
+            }
+
+            request.IsSuccess = true;
+
+            string requestFrom = new PayCenterClient(PayEnum.PayCenter).GetRequestHtml(request, false);
+            return Content(requestFrom);
+        }
+
+        /// <summary>
+        /// 支付宝异步处理
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AlipayNotify()
+        {
+            //调用远程方法
+
+            //var result = _payFactory.CheckNotifyData(PayEnum.Alipay, NotifyEnum.POST);
+            //if (result.Status)
+            //{
+            //    ConfirmOrder(result);
+            //}
+            //return Content(result.Message);
+            return View();
+        }
+
+
+        public ActionResult CmbReturn()
+        {
+            var entity = new PayCenterClient(PayEnum.CmbBank).GetRequestResult("支付宝同步");
+            //获取配置信息
+            PayCenterRequest request = TestDictionary.GetTestByPartnerId("");
+            request.ThirdPayResponse = entity;
+
+            if (!new PayCenterClient(PayEnum.CmbBank).SignVerify(request, entity.CmbBankResponse.Signature))
+            {
+
+            }
+
+            return View();
+        }
+
+        #endregion
+
+
+
+
+    }
+}
